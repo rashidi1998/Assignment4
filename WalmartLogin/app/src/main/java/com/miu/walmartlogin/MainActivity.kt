5844a97769d3c60300eead9cc1f527e7b1bb3430
Rashid <ismoilovrashid2@gmail.com>
2022-10-30 17:56:44 -0500

    added modified part

---
+++ WalmartLogin/app/src/main/java/com/miu/walmartlogin/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.miu.walmartlogin
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var userArr = arrayListOf(
+        User("Rashidjon","Ismoilov","rismoilov@gmail.com","123")
+        ,User("William","Smith","will@gmail.com","123")
+        ,User("Ozod","Tagoev","ozod@gmail.com","123")
+        ,User("Bill","Gates","bill@hotmail.com","123")
+        ,User("James","Bond","james@123.com","123"))
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //
+        btnSignIn.setOnClickListener{
+            var email = etEmail.text.toString()
+            var password = etPassword.text.toString()
+            for(u in userArr){
+                if(u.username == email && u.password == password){
+                    //Toast.makeText(this,email.toString(),Toast.LENGTH_LONG).show()
+                    //pass the user name to
+                    val intent = Intent(this,ShoppingCategory::class.java)
+                    intent.putExtra("username",email)
+                    startActivity(intent)
+                    break
+                }else{
+                    Toast.makeText(this,"Unable to login. Please try again!",Toast.LENGTH_LONG).show()
+                }
+            }
+
+        }
+        //Task5
+        var resultContracts = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            //callback to retrieve the result
+            //result object contains the intent and RESULT_OK or RESULT_CANCEL
+                result -> //what will do after getting result back
+            if(result.resultCode == Activity.RESULT_OK)
+                //userArr.add(result.data?.data as User)
+                //tv.text = result.data?.data.toString()//data means getting the intent
+                //val hi = result.data?.data.toString()
+                //println("========= Reg Act ==========: $result.data?.data.toString()")
+                Log.i("Reg Act:",result.data?.data.toString())
+            else
+                Log.i("Reg Act:","Failed to get Result")
+        }
+
+        btnCreate.setOnClickListener{
+            val intent = Intent(this,CreateAccount::class.java)
+            resultContracts.launch(intent)
+        }
+    }
+
+}
